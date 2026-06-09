1ea9430dc01caafd5403a683f3b9bfd976e932a1
Yean Khai <yeankhai98@gmail.com>
2026-06-09 23:05:37 +0800

    Remove the default local path to CLI executable file

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ DEFAULT_INTERVAL = 10 * 60  # 10 minutes in seconds
 
 MODEL_ID = "gemini-2.5-flash"
 
-LOCAL_WHISPER_CLI_PATH = "/Users/yeankhai/work/local_llm/whisper.cpp/build/bin/whisper-cli"
-LOCAL_WHISPER_MODEL_PATH = "/Users/yeankhai/work/local_llm/whisper.cpp/models/ggml-medium.en.bin"
+LOCAL_WHISPER_CLI_PATH = # Please insert your path to the whisper.cpp executable here
+LOCAL_WHISPER_MODEL_PATH = # Please insert your path to the whisper.cpp model file here
 
 LOCAL_TRANSCRIBE_TOKEN=True
